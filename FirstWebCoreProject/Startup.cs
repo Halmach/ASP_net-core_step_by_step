2b14c9cd1884134dc15a4c790c486eae8f01f63e
Khalmatov <large000@yandex.ru>
2022-08-09 23:23:20 +0300

    Маршрутизация в ASP.NET Core

--- FirstWebCoreProject/Startup.cs
+++ FirstWebCoreProject/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,11 +25,13 @@ namespace FirstWebCoreProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +59,11 @@ namespace FirstWebCoreProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHealthChecks("/healthz");
+                endpoints.MapGet("/test", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
 
         //    app.UseWelcomePage();
